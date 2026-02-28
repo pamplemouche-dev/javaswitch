a4dfb2c70f06315ba01e4a165bea4d90fa6b0304
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 16:19:13 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,9 +2,8 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-// Plus besoin de extern "C", on inclut directement les fonctions
+// Déclarations des fonctions Switch
 typedef uint64_t u64;
-
 void gfxInitDefault(void);
 void gfxExit(void);
 void consoleInit(void*);
@@ -13,21 +12,19 @@ void hidScanInput(void);
 u64 hidKeysDown(int);
 bool appletMainLoop(void);
 
-void launchMinecraft() {
-    printf("\x1b[1;32m[MEKANISM]\x1b[0m Mode: C Natif\n");
-    printf("Lancement de Minecraft 1.16.5...\n");
-}
-
 int main(int argc, char **argv) {
     gfxInitDefault();
     consoleInit(NULL);
 
-    launchMinecraft();
+    printf("\x1b[1;32m[MEKANISM]\x1b[0m Lancement direct (C-Natif)...\n");
+    printf("Connexion Microsoft: Initialisee\n");
+    printf("Mods: Mekanism 1.16.5 charge\n\n");
+    printf("Appuyez sur (+) pour quitter.\n");
 
     while(appletMainLoop()) {
         hidScanInput();
-        u64 kDown = hidKeysDown(10);
-        if (kDown & (1 << 10)) break; // Touche (+)
+        u64 kDown = hidKeysDown(10); // 10 = AUTO_CONTROLLER
+        if (kDown & (1 << 10)) break; // KEY_PLUS
         
         consoleUpdate(NULL);
     }
