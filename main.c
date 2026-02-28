b301354226a165d949b2061915a7f25cfc2ad392
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 16:21:41 +0100

    Update main.c

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdint.h>
 
-// Déclarations des fonctions Switch
+// Déclarations manuelles pour le Linker
 typedef uint64_t u64;
 void gfxInitDefault(void);
 void gfxExit(void);
@@ -13,19 +13,21 @@ u64 hidKeysDown(int);
 bool appletMainLoop(void);
 
 int main(int argc, char **argv) {
+    // Initialisation
     gfxInitDefault();
     consoleInit(NULL);
 
-    printf("\x1b[1;32m[MEKANISM]\x1b[0m Lancement direct (C-Natif)...\n");
-    printf("Connexion Microsoft: Initialisee\n");
-    printf("Mods: Mekanism 1.16.5 charge\n\n");
+    printf("\x1b[1;32m[MEKANISM]\x1b[0m Booting Minecraft...\n");
+    printf("Auth: Microsoft Integration\n");
     printf("Appuyez sur (+) pour quitter.\n");
 
+    // Boucle principale
     while(appletMainLoop()) {
         hidScanInput();
-        u64 kDown = hidKeysDown(10); // 10 = AUTO_CONTROLLER
-        if (kDown & (1 << 10)) break; // KEY_PLUS
-        
+        u64 kDown = hidKeysDown(10); // 10 = CONTROLLER_P1_AUTO
+
+        if (kDown & (1 << 10)) break; // (1 << 10) = KEY_PLUS
+
         consoleUpdate(NULL);
     }
 
