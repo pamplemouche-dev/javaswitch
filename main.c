fb8142b3b8ba915f38e4ed149bc8d354dab2fef2
Noam DA SILVA <dasilvanoam@savio-lambersart.com>
2026-02-28 17:07:27 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,45 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <switch.h>
-#include <stdio.h>
+
+// On définit les couleurs style Minecraft
+#define COLOR_BG_RED 34
+#define COLOR_BG_GREEN 34
+#define COLOR_BG_BLUE 34
 
 int main(int argc, char* argv[]) {
-    // Initialisation de la Switch et de la Vidéo
-    romfsInit();
-    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK);
+    // 1. Initialisation des services Switch
+    romfsInit(); // Pour charger des images internes plus tard
     
-    SDL_Window* window = SDL_CreateWindow("Mekanism Launcher", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-
-    // --- ACCÈS CARTE SD ---
-    FILE *file = fopen("sdmc:/MekanismLauncher/launcher.json", "r");
-    if (file) {
-        // Code pour lire tes mods ici
-        fclose(file);
+    // 2. Initialisation SDL2
+    if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_JOYSTICK) < 0) {
+        return -1;
     }
 
+    // Création de la fenêtre "Plein écran" Switch
+    SDL_Window* window = SDL_CreateWindow("Mekanism Launcher", 
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 1280, 720, 0);
+    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+
+    // 3. Test d'accès à la Carte SD
+    // On essaie d'ouvrir un fichier texte sur la SD
+    FILE *sdCard = fopen("sdmc:/MekanismLauncher/mods_list.txt", "r");
+    int sd_ok = (sdCard != NULL);
+    if (sdCard) fclose(sdCard);
+
+    // 4. Boucle de "Jeu"
     int running = 1;
     while (running) {
         SDL_Event event;
         while (SDL_PollEvent(&event)) {
             if (event.type == SDL_QUIT) running = 0;
+            
+            // Gestion des boutons de la manette
             if (event.type == SDL_JOYBUTTONDOWN) {
-                if (event.jbutton.button == 10) running = 0; // Touche (+)
+                if (event.jbutton.button == 10) running = 0; // Bouton (+) pour quitter
             }
         }
 
         // --- DESSIN DE L'INTERFACE ---
-        SDL_SetRenderDrawColor(renderer, 34, 34, 34, 255); // Gris foncé Minecraft
+        // Fond gris foncé
+        SDL_SetRenderDrawColor(renderer, COLOR_BG_RED, COLOR_BG_GREEN, COLOR_BG_BLUE, 255);
         SDL_RenderClear(renderer);
-        
-        // Ici on pourra ajouter : SDL_RenderCopy(renderer, texture_bouton, NULL, &rect);
+
+        // Si la SD est détectée, on dessine un rectangle vert
+        if (sd_ok) {
+            SDL_Rect rect = { 100, 100, 50, 50 };
+            SDL_SetRenderDrawColor(renderer, 0, 255, 0, 255);
+            SDL_RenderFillRect(renderer, &rect);
+        }
 
         SDL_RenderPresent(renderer);
     }
 
+    // Nettoyage
     SDL_DestroyRenderer(renderer);
     SDL_DestroyWindow(window);
     SDL_Quit();
     romfsExit();
+    
     return 0;
 }
